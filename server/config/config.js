162768b01afd3d1cde0a57f50abd65d85c5c6577
JOSE LUIS <jluiscaldera@hotmail.com>
2019-05-06 19:37:54 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://joseluis:eh6nEwiAbba9zxI2@cluster0-vz2q4.mongodb.net/cafe';
+    //urlDB = 'mongodb+srv://joseluis:eh6nEwiAbba9zxI2@cluster0-vz2q4.mongodb.net/cafe';
+    // COn vatiables de entorno de heroku
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
